1af2557dbaac16626919c433099bde5ab66b6c18
Nate Tan <nktan93@gmail.com>
2019-04-03 17:05:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
-
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(item, price, quantity = 1)
+    @total += price * quantity
+  end
+  
+  def apply_discount
+    @total -= @total * discount
+  end
+end
